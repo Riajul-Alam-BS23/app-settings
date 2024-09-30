41676bca9ebc80f6163e7da58e21c9631f0dc0a0
Md. Riajul Alam Bhuiyan <riajul.alam@brainstation-23.com>
2024-09-30 20:54:37 +0600

    refactor settings form

--- src/app/features/settings/settings-routing.module.ts
+++ src/app/features/settings/settings-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SettingsComponent } from './components/settings/settings.component';
 import { AccountComponent } from './components/settings/account/account.component';
-import { SetupComponent } from './components/settings/setup/setup/setup.component';
-import { TestingComponent } from './components/settings/setup/testing/testing.component';
 import { SetupFormComponent } from './components/settings/setup/setup-form/setup-form.component';
 
 const routes: Routes = [
@@ -14,14 +12,6 @@ const routes: Routes = [
       {
         path:'account',
         component:AccountComponent
-      },
-      {
-        path: 'setup',
-        component:SetupComponent
-      },
-      {
-        path:'testing',
-        component:TestingComponent
       },
       {
         path:'setup-form',
--- src/app/features/settings/settings.module.ts
+++ src/app/features/settings/settings.module.ts
@@ -3,30 +3,14 @@ import { NgModule } from '@angular/core';
 import { SettingsRoutingModule } from './settings-routing.module';
 import { SettingsComponent } from './components/settings/settings.component';
 import { SharedModule } from 'src/app/shared/shared.module';
-import { AlertContainerComponent } from './components/settings/setup/alert-container/alert-container.component';
-import { AlertItemComponent } from './components/settings/setup/alert-item/alert-item.component';
-import { TitleComponent } from './components/settings/setup/title/title.component';
-import { DropdownComponent } from './components/settings/setup/dropdown/dropdown.component';
-import { SettingsContainerComponent } from './components/settings/setup/settings-container/settings-container.component';
-import { SetupComponent } from './components/settings/setup/setup/setup.component';
 import { AccountComponent } from './components/settings/account/account.component';
-import { InputComponent } from './components/settings/setup/input/input.component';
-import { TestingComponent } from './components/settings/setup/testing/testing.component';
 import { SetupFormComponent } from './components/settings/setup/setup-form/setup-form.component';
 
 
 @NgModule({
   declarations: [
     SettingsComponent,
-    TitleComponent,
-    DropdownComponent,
-    AlertItemComponent,
-    AlertContainerComponent,
-    SettingsContainerComponent,
-    SetupComponent,
     AccountComponent,
-    InputComponent,
-    TestingComponent,
     SetupFormComponent
   ],
   imports: [
