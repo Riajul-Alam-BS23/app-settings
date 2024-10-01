17769435b5db54f74d2c5804b0af8e9bd14f7801
Md. Riajul Alam Bhuiyan <riajul.alam@brainstation-23.com>
2024-10-01 15:05:17 +0600

    refactor setting service

--- src/app/features/settings/services/settings.service.ts
+++ src/app/features/settings/services/settings.service.ts
@@ -8,31 +8,9 @@ import { Endpoints } from '../models/SetUp';
 })
 export class SettingsService {
   private baseUrl = environment.BASE_URL;
-  settings:any;
-  private keyValueStoreMap: Map<string[], string> = new Map();
 
   constructor(private http: HttpClient) {}
 
-  // updateKeyValueStore(path: any, value: any){
-  //     this.keyValueStoreMap.set(path,value);
-  // }
-
-  // updateOnApplyChanges(key: string): void {
-  //   let count=0;
-  //   this.keyValueStoreMap.forEach((value, currentKey) => {
-  //     if(currentKey[0]==key){
-  //       this.updateSettings(currentKey,value);
-  //       this.keyValueStoreMap.delete(currentKey);
-  //       count++;
-  //     }
-  //   });
-  //   if(count>0){
-  //     const update={
-  //         [key]: this.settings[key]
-  //     }
-  //     this.updateAlertsData(Endpoints[Endpoints.settings],update);
-  //   }
-  // }
   
   getAlertsData(type: string){
     const apiUrl = `${this.baseUrl}/${type}`;
@@ -46,20 +24,6 @@ export class SettingsService {
     const apiUrl = `${this.baseUrl}/${Endpoints[Endpoints.settings]}`;
     this.http.patch(apiUrl, update).subscribe();
   }
-  
-  // updateSettings(path: string[], newValue: string): void {
-  //   let target=this.settings;
-  //   for (let i= 0;i<path.length-1;i++) {
-  //       target = target[path[i]];
-  //   }
-  //   const finalKey = path[path.length - 1];
-  //   if(!target[finalKey].default){
-  //     target[finalKey].emails = newValue;
-  //   }else{
-  //     target[finalKey].default = newValue;
-  //   }
-  // }
-
 
 }
 
